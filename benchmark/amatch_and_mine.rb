587eb4ea8fc3ea8e116f585b1d0ca0b89f9c4fa7
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-10-06 10:32:09 +0900

    Added benchmark script.

---
+++ benchmark/amatch_and_mine.rb
@@ -0,0 +1,81 @@
+#!/usr/local/bin/ruby
+#
+# http://www.ruby-lang.org/ja/man/html/benchmark.html
+#
+require 'benchmark'
+require 'amatch'
+require '../lib/jarowinkler'
+
+looptimes = 10000
+
+names = [ "Aichi",
+          "Akita",
+         "Aomori",
+         "Chiba",
+         "Ehime",
+         "Fukui",
+         "Fukuoka",
+         "Fukushima",
+         "Gifu",
+         "Gunma",
+         "Hiroshima",
+         "Hokkaido",
+         "Hyogo",
+         "Ibaraki",
+         "Ishikawa",
+         "Iwate",
+         "Kagawa",
+         "Kagoshima",
+         "Kanagawa",
+         "Kochi",
+         "Kumamoto",
+         "Kyoto",
+         "Mie",
+         "Miyagi",
+         "Miyazaki",
+         "Nagano",
+         "Nagasaki",
+         "Nara",
+         "Niigata",
+         "Oita",
+         "Okayama",
+         "Okinawa",
+         "Osaka",
+         "Saga",
+         "Saitama",
+         "Shiga",
+         "Shimane",
+         "Shizuoka",
+         "Tochigi",
+         "Tokushima",
+         "Tokyo",
+         "Tottori",
+         "Toyama",
+         "Wakayama",
+         "Yamagata",
+         "Yamaguchi",
+         "Yamanashi" ]
+
+keyword = "Tokyo"
+
+puts "[Amatch]"
+puts Benchmark::CAPTION
+puts Benchmark.measure {
+  jarow = Amatch::JaroWinkler.new keyword
+  looptimes.times { |n|
+    names.map { |x|
+      jarow.match( x )
+    }
+  }
+}
+
+puts "[ThisModule]"
+puts Benchmark::CAPTION
+puts Benchmark.measure {
+  jarow = JaroWinkler::JaroWinkler.new
+  looptimes.times { |n|
+    names.map { |x|
+      jarow.getDistance( keyword, x )
+    }
+  }
+}
