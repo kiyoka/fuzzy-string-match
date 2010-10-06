c3b117fbfa318db7960fef95299f7553625ff304
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-10-06 14:22:01 +0900

    Jaro-Winkler was implemented in C language with RubyInline.

--- test/jarowinkler_spec.rb
+++ test/jarowinkler_spec.rb
@@ -24,9 +24,34 @@
 require 'jarowinkler'
 require 'amatch'
 
-describe JaroWinkler, "when some string distances are" do
+describe JaroWinkler, "when some string distances (Pure) are" do
   before do
-    @jarow = JaroWinkler::JaroWinkler.new
+    @jarow = JaroWinkler::JaroWinkler.new.create
+  end
+  it "should" do
+    @jarow.getDistance( "al",        "al"        ).should == 1.0
+    @jarow.getDistance( "martha",    "marhta"    ).should be_close( 0.9611, 0.0001 )
+    @jarow.getDistance( "jones",     "johnson"   ).should be_close( 0.8323, 0.0001 )
+    @jarow.getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_close( 0.9583, 0.0001 )
+    @jarow.getDistance( "dwayne",    "duane"     ).should be_close( 0.8400, 0.0001 )
+    @jarow.getDistance( "dixon",     "dicksonx"  ).should be_close( 0.8133, 0.0001 )
+    @jarow.getDistance( "fvie",      "ten"       ).should == 0.0
+    lambda {
+      d1 = @jarow.getDistance("zac ephron", "zac efron")
+      d2 = @jarow.getDistance("zac ephron", "kai ephron")
+      d1 > d2
+    }.should be_true
+    lambda {
+      d1 = @jarow.getDistance("brittney spears", "britney spears")
+      d2 = @jarow.getDistance("brittney spears", "brittney startzman")
+      d1 > d2
+    }.should be_true
+  end
+end
+
+describe JaroWinkler, "when some string distances (Native) are" do
+  before do
+    @jarow = JaroWinkler::JaroWinkler.new.create( :native )
   end
   it "should" do
     @jarow.getDistance( "al",        "al"        ).should == 1.0
