8215918ce627f6a5a5cbc2564c916d0b2b4e50bd
Kiyoka Nishiyama <kiyoka@sumibi.org>
2011-07-31 04:56:47 +0900

    Stop to use RSpec's obsolute method be_close. Used be_within(val).of(val) method chain instead of it.

--- test/fuzzystringmatch_spec.rb
+++ test/fuzzystringmatch_spec.rb
@@ -29,13 +29,13 @@ describe FuzzyStringMatch, "when some string distances (Pure) are" do
     @jarow = FuzzyStringMatch::JaroWinkler.new.create
   end
   it "should" do
-    @jarow.getDistance( "henka",     "henkan"    ).should be_close( 0.9722, 0.0001 )
+    @jarow.getDistance( "henka",     "henkan"    ).should be_within(0.0001).of(0.9722)
     @jarow.getDistance( "al",        "al"        ).should == 1.0
-    @jarow.getDistance( "martha",    "marhta"    ).should be_close( 0.9611, 0.0001 )
-    @jarow.getDistance( "jones",     "johnson"   ).should be_close( 0.8323, 0.0001 )
-    @jarow.getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_close( 0.9583, 0.0001 )
-    @jarow.getDistance( "dwayne",    "duane"     ).should be_close( 0.8400, 0.0001 )
-    @jarow.getDistance( "dixon",     "dicksonx"  ).should be_close( 0.8133, 0.0001 )
+    @jarow.getDistance( "martha",    "marhta"    ).should be_within(0.0001).of(0.9611)
+    @jarow.getDistance( "jones",     "johnson"   ).should be_within(0.0001).of(0.8323)
+    @jarow.getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_within(0.0001).of(0.9583)
+    @jarow.getDistance( "dwayne",    "duane"     ).should be_within(0.0001).of(0.8400)
+    @jarow.getDistance( "dixon",     "dicksonx"  ).should be_within(0.0001).of(0.8133)
     @jarow.getDistance( "fvie",      "ten"       ).should == 0.0
     lambda {
       d1 = @jarow.getDistance("zac ephron", "zac efron")
@@ -55,13 +55,13 @@ describe FuzzyStringMatch, "when some string distances (Native) are" do
     @jarow = FuzzyStringMatch::JaroWinkler.new.create( :native )
   end
   it "should" do
-    @jarow.getDistance( "henka",     "henkan"    ).should be_close( 0.9722, 0.0001 )
+    @jarow.getDistance( "henka",     "henkan"    ).should be_within(0.0001).of(0.9722)
     @jarow.getDistance( "al",        "al"        ).should == 1.0
-    @jarow.getDistance( "martha",    "marhta"    ).should be_close( 0.9611, 0.0001 )
-    @jarow.getDistance( "jones",     "johnson"   ).should be_close( 0.8323, 0.0001 )
-    @jarow.getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_close( 0.9583, 0.0001 )
-    @jarow.getDistance( "dwayne",    "duane"     ).should be_close( 0.8400, 0.0001 )
-    @jarow.getDistance( "dixon",     "dicksonx"  ).should be_close( 0.8133, 0.0001 )
+    @jarow.getDistance( "martha",    "marhta"    ).should be_within(0.0001).of(0.9611)
+    @jarow.getDistance( "jones",     "johnson"   ).should be_within(0.0001).of(0.8323)
+    @jarow.getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_within(0.0001).of(0.9583)
+    @jarow.getDistance( "dwayne",    "duane"     ).should be_within(0.0001).of(0.8400)
+    @jarow.getDistance( "dixon",     "dicksonx"  ).should be_within(0.0001).of(0.8133)
     @jarow.getDistance( "fvie",      "ten"       ).should == 0.0
     lambda {
       d1 = @jarow.getDistance("zac ephron", "zac efron")
@@ -87,13 +87,13 @@ describe FuzzyStringMatch, "when some UTF8 string distances (Pure) are" do
     @jarow = FuzzyStringMatch::JaroWinkler.new.create
   end
   it "should" do
-    @jarow.getDistance( "ａｌ",              "ａｌ"        ).should == 1.0
-    @jarow.getDistance( "ｍａｒｔｈａ",      "ｍａｒｈｔａ"    ).should be_close( 0.9611, 0.0001 )
-    @jarow.getDistance( "ｊｏｎｅｓ",        "ｊｏｈｎｓｏｎ"   ).should be_close( 0.8323, 0.0001 )
-    @jarow.getDistance( "ａｂｃｖｗｘｙｚ",  "ｃａｂｖｗｘｙｚ"  ).should be_close( 0.9583, 0.0001 )
-    @jarow.getDistance( "ｄｗａｙｎｅ",      "ｄｕａｎｅ"     ).should be_close( 0.8400, 0.0001 )
-    @jarow.getDistance( "ｄｉｘｏｎ",        "ｄｉｃｋｓｏｎｘ"  ).should be_close( 0.8133, 0.0001 )
-    @jarow.getDistance( "ｆｖｉｅ",          "ｔｅｎ"       ).should == 0.0
+    @jarow.getDistance( "ａｌ",              "ａｌ"             ).should == 1.0
+    @jarow.getDistance( "ｍａｒｔｈａ",      "ｍａｒｈｔａ"     ).should be_within(0.0001).of(0.9611)
+    @jarow.getDistance( "ｊｏｎｅｓ",        "ｊｏｈｎｓｏｎ"   ).should be_within(0.0001).of(0.8323)
+    @jarow.getDistance( "ａｂｃｖｗｘｙｚ",  "ｃａｂｖｗｘｙｚ" ).should be_within(0.0001).of(0.9583)
+    @jarow.getDistance( "ｄｗａｙｎｅ",      "ｄｕａｎｅ"       ).should be_within(0.0001).of(0.8400)
+    @jarow.getDistance( "ｄｉｘｏｎ",        "ｄｉｃｋｓｏｎｘ" ).should be_within(0.0001).of(0.8133)
+    @jarow.getDistance( "ｆｖｉｅ",          "ｔｅｎ"           ).should == 0.0
     lambda {
       d1 = @jarow.getDistance("ｚａｃ　ｅｐｈｒｏｎ", "ｚａｃ　ｅｆｒｏｎ")
       d2 = @jarow.getDistance("ｚａｃ　ｅｐｈｒｏｎ", "ｋａｉ　ｅｐｈｒｏｎ")
@@ -104,26 +104,26 @@ describe FuzzyStringMatch, "when some UTF8 string distances (Pure) are" do
       d2 = @jarow.getDistance("ｂｒｉｔｔｎｅｙ　ｓｐｅａｒｓ", "ｂｒｉｔｔｎｅｙ　ｓｔａｒｔｚｍａｎ")
       d1 > d2
     }.should be_true
-    @jarow.getDistance( "スパゲティー",              "スパゲッティー"    ).should be_close( 0.9666, 0.0001 )
-    @jarow.getDistance( "スパゲティー",              "スパゲティ"        ).should be_close( 0.9722, 0.0001 )
-    @jarow.getDistance( "スティービー・ワンダー",    "スピーディー・ワンダー"     ).should be_close( 0.8561, 0.0001 )
-    @jarow.getDistance( "マイケル・ジャクソン",      "ジャイケル・マクソン"       ).should be_close( 0.8000, 0.0001 )
-    @jarow.getDistance( "まつもとゆきひろ",          "まつもとひろゆき"           ).should be_close( 0.9500, 0.0001 )
-    @jarow.getDistance( "クライエント",              "クライアント"      ).should be_close( 0.9222, 0.0001 )
-    @jarow.getDistance( "サーバー",                  "サーバ"            ).should be_close( 0.9416, 0.0001 )
+    @jarow.getDistance( "スパゲティー",              "スパゲッティー"             ).should be_within(0.0001).of(0.9666)
+    @jarow.getDistance( "スパゲティー",              "スパゲティ"                 ).should be_within(0.0001).of(0.9722)
+    @jarow.getDistance( "スティービー・ワンダー",    "スピーディー・ワンダー"     ).should be_within(0.0001).of(0.8561)
+    @jarow.getDistance( "マイケル・ジャクソン",      "ジャイケル・マクソン"       ).should be_within(0.0001).of(0.8000)
+    @jarow.getDistance( "まつもとゆきひろ",          "まつもとひろゆき"           ).should be_within(0.0001).of(0.9500)
+    @jarow.getDistance( "クライエント",              "クライアント"               ).should be_within(0.0001).of(0.9222)
+    @jarow.getDistance( "サーバー",                  "サーバ"                     ).should be_within(0.0001).of(0.9416)
   end
 end
 
 
 describe Amatch, "when use Amatch gem, results are" do
   it "should" do
-    amatch_getDistance( "henka",     "henkan"    ).should be_close( 0.9666, 0.0001 )  ## amatch's result value is different from lucene version.
+    amatch_getDistance( "henka",     "henkan"    ).should be_within(0.0001).of(0.9666)  ## amatch's result value is different from lucene version.
     amatch_getDistance( "al",        "al"        ).should == 1.0
-    amatch_getDistance( "martha",    "marhta"    ).should be_close( 0.9611, 0.0001 )
-    amatch_getDistance( "jones",     "johnson"   ).should be_close( 0.8323, 0.0001 )
-    amatch_getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_close( 0.9583, 0.0001 )
-    amatch_getDistance( "dwayne",    "duane"     ).should be_close( 0.8400, 0.0001 )
-    amatch_getDistance( "dixon",     "dicksonx"  ).should be_close( 0.8133, 0.0001 )
+    amatch_getDistance( "martha",    "marhta"    ).should be_within(0.0001).of(0.9611)
+    amatch_getDistance( "jones",     "johnson"   ).should be_within(0.0001).of(0.8323)
+    amatch_getDistance( "abcvwxyz",  "cabvwxyz"  ).should be_within(0.0001).of(0.9583)
+    amatch_getDistance( "dwayne",    "duane"     ).should be_within(0.0001).of(0.8400)
+    amatch_getDistance( "dixon",     "dicksonx"  ).should be_within(0.0001).of(0.8133)
     amatch_getDistance( "fvie",      "ten"       ).should == 0.0
     lambda {
       d1 = amatch_getDistance("zac ephron", "zac efron")
