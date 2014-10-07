b93c141782d61f02cef0714c6036c507965bfe91
Kiyoka Nishiyama <kiyoka@sumibi.org>
2014-10-08 00:19:35 +0900

    Write console result of LuceneSample.java execution.

--- original/LuceneSample/src/LuceneSample.java
+++ original/LuceneSample/src/LuceneSample.java
@@ -23,6 +23,16 @@ public class LuceneSample {
 		  displayDistance(sd, "dwayne", "duane");
 		  displayDistance(sd, "dixon", "dicksonx");
 		  displayDistance(sd, "fvie", "ten");
+		  
+		  // This program displays:
+		  //   str1[henka] str2[henkan] d=0.972222
+		  //   str1[al] str2[al] d=1.000000
+		  //   str1[martha] str2[marhta] d=0.961111
+		  //   str1[jones] str2[johnson] d=0.832381
+		  //   str1[abcvwxyz] str2[cabvwxyz] d=0.958333
+		  //   str1[dwayne] str2[duane] d=0.840000
+		  //   str1[dixon] str2[dicksonx] d=0.813333
+		  //   str1[fvie] str2[ten] d=0.000000
 	}
 
 }
